2e20a2d79145d7d80199a4f09163956e30565d4f
Ozzonez <recondrone7@gmail.com>
2020-06-11 23:50:10 +0200

    added scheduling tools

--- utils.h
+++ utils.h
@@ -5,10 +5,43 @@
 #ifndef SCZR_UTILS_H
 #define SCZR_UTILS_H
 
+//#include <math.h>
+#include <linux/kernel.h>
+#include <linux/unistd.h>
 #include <time.h>
+#include <linux/types.h>
 #include <stdlib.h>
 #include <stdio.h>
-//#include <math.h>
+#include <pthread.h>
+#include <string.h>
+#include <linux/sched.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#ifdef __x86_64__
+#define __NR_sched_setattr		314
+#define __NR_sched_getattr		315
+#endif
+
+#ifdef __i386__
+#define __NR_sched_setattr		351
+#define __NR_sched_getattr		352
+#endif
+
+#ifdef __arm__
+#define __NR_sched_setattr		380
+#define __NR_sched_getattr		381
+#endif
+
+#define SF_SIG_RORUN		2
+#define SF_SIG_DMISS		4
+#define SF_BWRECL_DL		8
+#define SF_BWRECL_RT		16
+#define SF_BWRECL_OTH		32
+
+#define RLIMIT_DLDLINE		16
+#define RLIMIT_DLRTIME		17
 
 char* hashMessage(char*);
 
@@ -17,6 +50,40 @@ typedef struct{
     char* message;
     //int time; - 3 timery
 }message;
+
+struct sched_attr {
+	__u32 size;
+	
+	__u32 sched_policy;
+	__u64 sched_flags;
+	
+	/* SCHED_NORMAL, SCHED_BATCH */
+	__s32 sched_nice;
+	
+	/* SCHED_FIFO, SCHED_RR */
+	__u32 sched_priority;
+	
+	/* SCHED_DEADLINE */
+	__u64 sched_runtime;
+	__u64 sched_deadline;
+	__u64 sched_period;
+};
+
+int sched_setattr(pid_t pid,
+		const struct sched_attr *attr,
+		unsigned int flags)
+{
+	return syscall(__NR_sched_setattr, pid, attr, flags);
+}
+
+int sched_getattr(pid_t pid,
+		struct sched_attr *attr,
+		unsigned int size,
+		unsigned int flags)
+{
+	return syscall(__NR_sched_getattr, pid, attr, size, flags);
+}
+
 
 int poww(int,int);
 void generateMessage(int length,char * message)
