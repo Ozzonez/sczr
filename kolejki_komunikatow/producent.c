dc990e5ac94a613cc1cab7bda2f1146e5eb78f13
mkalino1 <63191350+mkalino1@users.noreply.github.com>
2020-06-12 21:25:37 +0200

    update producent

--- kolejki_komunikatow/producent.c
+++ kolejki_komunikatow/producent.c
@@ -1,41 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/time.h>
-#include <mqueue.h>
-#include "utils.h"
+#include "../utils.h"
 #include "common.h"
 
 
 int main(int argc, char **argv)
 {
-    /* open the mail queue */
+	// Trzeba usuwac pozostale w systemie kolejki o tej samej nazwie, zeby nie bylo bledow
+	if(mq_unlink(QUEUE_NAME) == 0)
+		fprintf(stdout, "Message queue %s removed from system.\n", QUEUE_NAME);
+
+    /* initialize the queue attributes */
     mqd_t mq;
-    mq = mq_open(QUEUE_NAME, O_WRONLY);
+    struct mq_attr attr;
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = sizeof(Komunikat);
+    attr.mq_curmsgs = 0;
+
+    /* create the message queue */
+    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, 0644, &attr);
     CHECK((mqd_t)-1 != mq);
 
-
-    printf("Send to server (enter \"exit\" to stop it):\n");
+	struct timeval tv;
+	char  result[]="aaaaa";
 
 	Komunikat kom;
-	strcpy(kom.dane, "jp2g");
-	struct timeval tv;
 
 
-    do {
-        generateMessage(4, kom.dane);
+    while(1) {
+        ssize_t bytes_read;
+
+        /* receive the message */
+        bytes_read = mq_receive(mq, (char *) &kom, sizeof(Komunikat), NULL);
+        CHECK(bytes_read >= 0);
+        printf("Received: %d\n", kom.dane);
+
+	//Obliczanie czasu przesylania 
 	gettimeofday(&tv, NULL);
-	kom.czasDostarczenia = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+	kom.czasDostarczenia = ((tv.tv_sec) * 1000 + (tv.tv_usec) / 1000) - kom.czasDostarczenia;
+	printf(" time: %ld\n", kom.czasDostarczenia);
 
-        /* send the message */
-        CHECK(0 <= mq_send(mq, (const char *) &kom, sizeof(Komunikat), 0));
+	//Przetwarzanie danych
+	decryptMessage(kom.dane, 4, result);
 
-    } while (strncmp(kom.dane, MSG_STOP, strlen(MSG_STOP)));
+
+	printf("%s\n", result);
+    }
 
     /* cleanup */
     CHECK((mqd_t)-1 != mq_close(mq));
+    CHECK((mqd_t)-1 != mq_unlink(QUEUE_NAME));
 
     return 0;
 }
