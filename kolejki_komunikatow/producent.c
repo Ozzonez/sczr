701e027302b77e53711871a2ed76dd03c0069d34
mkalino1 <63191350+mkalino1@users.noreply.github.com>
2020-06-12 21:27:37 +0200

    update producent

--- kolejki_komunikatow/producent.c
+++ kolejki_komunikatow/producent.c
@@ -1,55 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/time.h>
+#include <mqueue.h>
 #include "../utils.h"
 #include "common.h"
 
 
 int main(int argc, char **argv)
 {
-	// Trzeba usuwac pozostale w systemie kolejki o tej samej nazwie, zeby nie bylo bledow
-	if(mq_unlink(QUEUE_NAME) == 0)
-		fprintf(stdout, "Message queue %s removed from system.\n", QUEUE_NAME);
-
-    /* initialize the queue attributes */
+    /* open the mail queue */
     mqd_t mq;
-    struct mq_attr attr;
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = sizeof(Komunikat);
-    attr.mq_curmsgs = 0;
-
-    /* create the message queue */
-    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, 0644, &attr);
+    mq = mq_open(QUEUE_NAME, O_WRONLY);
     CHECK((mqd_t)-1 != mq);
 
-	struct timeval tv;
-	char  result[]="aaaaa";
+
+    printf("Send to server (enter \"exit\" to stop it):\n");
 
 	Komunikat kom;
+	kom.dane = 2077;
+	struct timeval tv;
 
 
     while(1) {
-        ssize_t bytes_read;
-
-        /* receive the message */
-        bytes_read = mq_receive(mq, (char *) &kom, sizeof(Komunikat), NULL);
-        CHECK(bytes_read >= 0);
-        printf("Received: %d\n", kom.dane);
-
-	//Obliczanie czasu przesylania 
+        kom.dane = generateMessage(4);
 	gettimeofday(&tv, NULL);
-	kom.czasDostarczenia = ((tv.tv_sec) * 1000 + (tv.tv_usec) / 1000) - kom.czasDostarczenia;
-	printf(" time: %ld\n", kom.czasDostarczenia);
+	kom.czasDostarczenia = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
 
-	//Przetwarzanie danych
-	decryptMessage(kom.dane, 4, result);
-
-
-	printf("%s\n", result);
+        /* send the message */
+        CHECK(0 <= mq_send(mq, (const char *) &kom, sizeof(Komunikat), 0));
     }
 
     /* cleanup */
     CHECK((mqd_t)-1 != mq_close(mq));
-    CHECK((mqd_t)-1 != mq_unlink(QUEUE_NAME));
 
     return 0;
 }
